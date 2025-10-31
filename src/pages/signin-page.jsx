676fd4cd17069160cb7925db2d6d05402ce9269a
Davi Fernandes <davi.fer159@gmail.com>
2025-10-31 19:11:59 -0300

    feat: add validation in singin form

--- src/pages/signin-page.jsx
+++ src/pages/signin-page.jsx
@@ -1,4 +1,7 @@
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useForm } from "react-hook-form";
 import { Link } from "react-router-dom";
+import z from "zod";
 
 import InputPassword from "@/components/input-password";
 import { Button } from "@/components/ui/button";
@@ -10,26 +13,86 @@ import {
   CardHeader,
   CardTitle,
 } from "@/components/ui/card";
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormMessage,
+} from "@/components/ui/form";
 import { Input } from "@/components/ui/input";
 
+const signinSchema = z.object({
+  email: z
+    .string()
+    .email("E-mail inválido")
+    .trim()
+    .nonempty("O e-mail é obrigatório"),
+  password: z
+    .string()
+    .nonempty("A senha é obrigatória")
+    .min(6, "A senha deve ter no mínimo 6 caracteres"),
+});
+
+const signin = (data) => {
+  console.log(data);
+};
+
 const SignIn = () => {
+  const form = useForm({
+    resolver: zodResolver(signinSchema),
+    defaultValues: {
+      email: "",
+      password: "",
+    },
+  });
+
   return (
     <div className="flex h-screen w-screen flex-col items-center justify-center gap-4">
-      <Card className="w-full max-w-[500px]">
-        <CardHeader className="text-center">
-          <CardTitle className="text-2xl">Entrar na sua conta</CardTitle>
-          <CardDescription className="text-muted-foreground text-sm">
-            Insira seus dados abaixo.
-          </CardDescription>
-        </CardHeader>
-        <CardContent className="space-y-4">
-          <Input placeholder="Digite seu e-mail" />
-          <InputPassword placeholder="Digite sua senha" />
-        </CardContent>
-        <CardFooter>
-          <Button className="w-full">Entrar</Button>
-        </CardFooter>
-      </Card>
+      <Form {...form}>
+        <form onSubmit={form.handleSubmit(signin)}>
+          <Card className="w-[500px]">
+            <CardHeader className="text-center">
+              <CardTitle className="text-2xl">Entrar na sua conta</CardTitle>
+              <CardDescription className="text-muted-foreground text-sm">
+                Insira seus dados abaixo.
+              </CardDescription>
+            </CardHeader>
+            <CardContent className="space-y-4">
+              <FormField
+                control={form.control}
+                name="email"
+                render={({ field }) => (
+                  <FormItem>
+                    <FormControl>
+                      <Input placeholder="Digite seu e-mail" {...field} />
+                    </FormControl>
+                    <FormMessage />
+                  </FormItem>
+                )}
+              />
+              <FormField
+                control={form.control}
+                name="password"
+                render={({ field }) => (
+                  <FormItem>
+                    <FormControl>
+                      <InputPassword
+                        placeholder="Digite sua senha"
+                        {...field}
+                      />
+                    </FormControl>
+                    <FormMessage />
+                  </FormItem>
+                )}
+              />
+            </CardContent>
+            <CardFooter>
+              <Button className="w-full">Entrar</Button>
+            </CardFooter>
+          </Card>
+        </form>
+      </Form>
       <p className="text-muted-foreground text-sm">
         Ainda não possui uma conta?
         <Button variant="link" className="p-1" asChild>
