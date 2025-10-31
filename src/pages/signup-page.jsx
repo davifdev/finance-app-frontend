c28c98e6a9527c3cf19ee72d8dab59d5611fda55
Davi Fernandes <davi.fer159@gmail.com>
2025-10-31 18:04:39 -0300

    chore: create schema signup form

--- src/pages/signup-page.jsx
+++ src/pages/signup-page.jsx
@@ -1,4 +1,7 @@
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useForm } from "react-hook-form";
 import { Link } from "react-router-dom";
+import z from "zod";
 
 import InputPassword from "@/components/input-password";
 import { Button } from "@/components/ui/button";
@@ -13,7 +16,45 @@ import {
 import { Checkbox } from "@/components/ui/checkbox";
 import { Input } from "@/components/ui/input";
 
+const signupSchema = z
+  .object({
+    name: z.string().trim().nonempty("O nome é obrigatório."),
+    lastName: z.string().trim().nonempty("O sobrenome é obrigatório."),
+    email: z.email().trim().nonempty("O e-mail é obrigatório."),
+    password: z
+      .string()
+      .trim()
+      .nonempty("A senha é obrigatória")
+      .min(6, "A senha deve ter no mínimo 6 caracteres"),
+    confirmPassword: z
+      .string()
+      .trim()
+      .nonempty("A confirmação de senha é obrigatória")
+      .min(6, "A confirmação de senha deve ter no mínimo 6 caracteres"),
+    terms: z.boolean().refine((value) => value === true, {
+      message: "Você precisa aceitar os termos.",
+    }),
+  })
+  .refine((data) => data.password === data.confirmPassword, {
+    message: "As senhas devem ser iguais",
+    path: ["confirmPassword"],
+  });
+
 const SignUp = () => {
+  const form = useForm({
+    resolver: zodResolver(signupSchema),
+    defaultValues: {
+      name: "",
+      lastName: "",
+      email: "",
+      password: "",
+      confirmPassword: "",
+      terms: false,
+    },
+  });
+
+  console.log(form);
+
   return (
     <div className="flex h-screen w-screen flex-col items-center justify-center gap-4">
       <Card className="w-full max-w-[500px]">
