c8bb67056940e0c551f1dda4c49660a5ab3b1bf7
Davi Fernandes <davi.fer159@gmail.com>
2025-11-03 13:19:17 -0300

    docs: adding documentation for functions that call the API

--- src/services/user.js
+++ src/services/user.js
@@ -1,6 +1,16 @@
 import { protectedApi, publicApi } from "@/lib/axios";
 
 export const UserService = {
+  /**
+   * Criação de um novo Usuário
+   * @param {string} data.name - Nome do usuário
+   * @param {string} data.lastname - Sobrenome do usuário
+   * @param {string} data.email - E-mail do usuário
+   * @param {stirng} data.password - Senha do usuário
+   * @param {string} data.confirmPassword - Confirmação de Senha
+   * @return {Object} Usuário criado
+   * @return {string} data.tokens - Tokens de autenticação
+   */
   signup: async (data) => {
     const response = await publicApi.post("/users", {
       first_name: data.name,
@@ -17,6 +27,13 @@ export const UserService = {
       tokens: response.data.tokens,
     };
   },
+  /**
+   * Criação de um novo Usuário
+   * @param {string} data.email - E-mail do usuário
+   * @param {stirng} data.password - Senha do usuário
+   * @return {Object} Usuário Logado
+   * @return {string} data.tokens - Tokens de autenticação
+   */
   signin: async (data) => {
     const response = await publicApi.post("/users/login", {
       email: data.email,
@@ -30,6 +47,10 @@ export const UserService = {
       tokens: response.data.tokens,
     };
   },
+  /**
+   * Retorna usuário autenticado
+   * @return {Object} Usuário autenticado
+   */
   me: async () => {
     const response = await protectedApi("/users/me");
     return {
